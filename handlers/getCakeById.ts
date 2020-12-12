57cc481c87f0d4ac728c47a27511bb651aa700a2
jon-wade <jonwadeuk@gmail.com>
2020-12-12 13:23:45 +0000

    initial commit

---
+++ handlers/getCakeById.ts
@@ -0,0 +1,16 @@
+import { Handler, Context, Callback, APIGatewayProxyEvent } from 'aws-lambda';
+import { Response, Cake } from '../types'
+
+const getCakeById: Handler = (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
+    const response: Response = {
+        statusCode: 200,
+        body: 'OK',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        }
+    };
+    callback(undefined, response);
+};
+
+export { getCakeById }
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { getCakes } from './handlers/getCakes'
+import { getCakeById } from './handlers/getCakeById'
+import { deleteCakeById } from './handlers/deleteCakeById'
+import { postCake } from './handlers/postCake'
+import { putCakeById } from './handlers/putCakeById'
+
+export { getCakes, getCakeById, deleteCakeById, postCake, putCakeById }
